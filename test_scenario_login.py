c87c76b06647ca8b10f558fb77640ff2a671abc5
Wakhid_k <kurniawan.wk48@gmail.com>
2022-09-01 09:56:13 +0700

    Update test_scenario_login.py

--- test_scenario_login.py
+++ test_scenario_login.py
@@ -83,7 +83,7 @@ class TestLogin(unittest.TestCase):  # TEST SCENARIO
         self.assertIn('not found', text_atas)
         self.assertEqual(text_bawah, 'Email atau Password Anda Salah')
 
-    def test_a_empty_form_register(self): 
+    def test_e_empty_form_register(self): 
         # steps
         browser = self.browser #buka web browser
         browser.get("http://barru.pythonanywhere.com/daftar") # buka situs
@@ -100,7 +100,7 @@ class TestLogin(unittest.TestCase):  # TEST SCENARIO
         self.assertIn(text_atas)
         self.assertEqual(text_bawah)
 
-    def test_a_success_or_exists_register(self): 
+    def test_f_insert_or_exists_register(self): 
         # steps
         browser = self.browser #buka web browser
         browser.get("http://barru.pythonanywhere.com/daftar") # buka situs
@@ -110,6 +110,29 @@ class TestLogin(unittest.TestCase):  # TEST SCENARIO
         browser.find_element(By.XPATH,"/html/body/div/div[1]/form/input[1]").send_keys("Wakhid Kurniawan") # isi email
         time.sleep(1)
         browser.find_element(By.XPATH,"/html/body/div/div[1]/form/input[2]").send_keys("kabirhari11@gmail.com") # isi email
+        time.sleep(1)
+        browser.find_element(By.CSS_SELECTOR,"input#password_register").send_keys("kabir") # isi password
+        time.sleep(1)
+        browser.find_element(By.ID,"signup_register").click() # klik tombol sign in
+        time.sleep(1)
+
+        # validasi
+        text_atas = browser.find_element(By.ID,"swal2-title").text
+        text_bawah = browser.find_element(By.ID,"swal2-content").text
+
+        self.assertIn(text_atas)
+        self.assertEqual(text_bawah)
+
+    def test_g_register_invalid_email(self): 
+        # steps
+        browser = self.browser #buka web browser
+        browser.get("http://barru.pythonanywhere.com/daftar") # buka situs
+        time.sleep(3)
+        browser.find_element(By.ID,"signUp").click() # klik tombol sign up
+        time.sleep(1)
+        browser.find_element(By.XPATH,"/html/body/div/div[1]/form/input[1]").send_keys("Wakhid Kurniawan") # isi email
+        time.sleep(1)
+        browser.find_element(By.XPATH,"/html/body/div/div[1]/form/input[2]").send_keys("kabirhari11") # isi email
         time.sleep(1)
         browser.find_element(By.CSS_SELECTOR,"input#password_register").send_keys("kabir") # isi password
         time.sleep(1)
